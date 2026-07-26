a589eab5d5394ce42915a7078fcadffef9333e5f
khurram-uworx <khurram.aziz@uworx.co.uk>
2026-07-27 01:50:18 +0500

    cleanup

--- tests/Nivara.Tests/AutoDiff/PyTorchReferenceTests.cs
+++ tests/Nivara.Tests/AutoDiff/PyTorchReferenceTests.cs
@@ -1,7 +1,5 @@
-using System.Text.Json;
 using Nivara.AutoDiff;
 using Nivara.AutoDiff.Nn;
-using Nivara.AutoDiff.Nn.Functional;
 using Nivara.AutoDiff.Operations;
 using Nivara.AutoDiff.Utilities;
 using NUnit.Framework;
