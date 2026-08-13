39860e762f62668234e3ab91e563ae048c43b245
khurram-uworx <khurram.aziz@uworx.co.uk>
2026-08-13 20:11:08 +0500

    test(autodiff): dispatch multi-input forward through IMultipleInputModule<T> (#202)
    The #203 regression test dispatched MultiheadAttention.Forward(input,
    paddingMask) through a Module<float> reference. Multi-input forward is now an
    interface capability, so the test re-points at IMultipleInputModule<float> and
    gains a VAE interface-dispatch sibling. The interface extends IDisposable so
    both modules dispose through it in using declarations.

--- src/Nivara/AutoDiff/Nn/IMultipleInputModule.cs
+++ src/Nivara/AutoDiff/Nn/IMultipleInputModule.cs
@@ -2,7 +2,7 @@ using System.Numerics;
 
 namespace Nivara.AutoDiff.Nn;
 
-public interface IMultipleInputModule<T> where T : struct, IFloatingPointIeee754<T>
+public interface IMultipleInputModule<T> : IDisposable where T : struct, IFloatingPointIeee754<T>
 {
     ReverseGradTensor<T> Forward(ReverseGradTensor<T> input1, ReverseGradTensor<T> input2);
 }
